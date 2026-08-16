0aadf07a7ebecd8aad67c30f799feac60af2804b
Edward Pearson <s3844470@student.rmit.edu.au>
2026-08-16 15:49:04 +1000

    Add desktop icon column with folder/document glyphs (All custom css)

--- src/components/Desktop/Desktop.tsx
+++ src/components/Desktop/Desktop.tsx
@@ -1,5 +1,18 @@
+import { getNode } from '../../data'
+import { DesktopIcon } from './DesktopIcon'
 import styles from './Desktop.module.css'
 
+const projectsNode = getNode('projects')!
+const readmeNode = getNode('readme')!
+
 export function Desktop() {
-  return <div className={styles.desktop} />
+  return (
+    <div className={styles.desktop}>
+      <div className={styles.iconColumn}>
+        <DesktopIcon label={projectsNode.name} glyph="folder" />
+        <DesktopIcon label="About Me" glyph="document" />
+        <DesktopIcon label={readmeNode.name} glyph="document" />
+      </div>
+    </div>
+  )
 }
