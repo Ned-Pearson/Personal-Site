fce4da9e37ab108af1f1f83d6e8b8a19c04933e3
Edward Pearson <s3844470@student.rmit.edu.au>
2026-09-02 17:44:05 +1000

    Add Vercel Web Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { Analytics } from '@vercel/analytics/react'
 import { Desktop } from './components/Desktop/Desktop'
 import { Mobile } from './components/Mobile/Mobile'
 import { useIsMobile } from './hooks/useIsMobile'
 
 function App() {
   const isMobile = useIsMobile()
-  return isMobile ? <Mobile /> : <Desktop />
+  return (
+    <>
+      {isMobile ? <Mobile /> : <Desktop />}
+      <Analytics />
+    </>
+  )
 }
 
 export default App
